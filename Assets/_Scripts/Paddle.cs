ed3c1b5448c14e849b85bd0f2f8582ab3594b8ab
CharlesJuniorF <161252667+CharlesJuniorF@users.noreply.github.com>
2025-09-26 15:42:17 -0400

    Finished video 3
    Added wall collisions
    Added bouncing mechanic

--- Assets/_Scripts/Paddle.cs
+++ Assets/_Scripts/Paddle.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Paddle : MonoBehaviour
 {
     public float speed = .2f;
+    public float maxZ = 12.0f;
     public KeyCode upKey;
     public KeyCode downKey;
 
@@ -14,10 +15,20 @@ public class Paddle : MonoBehaviour
         if (Input.GetKey(upKey))
         {
             transform.position += new Vector3(0, 0, speed * Time.deltaTime);
+            if (transform.position.z > maxZ)
+            {
+                transform.position = new Vector3(
+                    transform.position.x, transform.position.y, maxZ);
+            }
         }
         else if (Input.GetKey(downKey)) 
         {
             transform.position += new Vector3(0, 0, -(speed * Time.deltaTime));
+            if (transform.position.z < -(maxZ))
+            {
+                transform.position = new Vector3(
+                    transform.position.x, transform.position.y, -(maxZ));
+            }
         }
     }
 }
